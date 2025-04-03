60a471165e4d42b7cbb9c399ce06f75e84ec15e7
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-03 16:36:51 -0300

    Punto 2 Finalizado

--- 1_NumeroPositivoNegativo.js
+++ 1_NumeroPositivoNegativo.js
@@ -1,1 +1,40 @@
 // 1. Leer una variable N de tipo entero e informar si el numero es positivo, negativo o cero y también si es par, impar o cero. El número cero no es ni positivo ni negativo ni par ni impar.
+
+//import PromptSync from "prompt-sync"
+
+//import chalk from "chalk"
+
+// const PromptSync = require("prompt-sync")
+
+import PromptSync from "prompt-sync"
+import chalk from "chalk"
+
+const prompt = PromptSync()
+// --------------------------------------------------------
+
+const esParImpar = (Num) => {
+    if (N === 0){
+      return console.log(chalk.yellowBright(`El número ingresado es ${N}.\nDicho dígito no es par, tampoco impar; no es positivo ni negativo.`))
+    } else if (N % 2 === 0) {
+       return console.log(chalk.blueBright(`El número ingresado es ${N}.\nDicho dígito es par.`))
+    } else if (N % 2 !== 0) {
+      return console.log(chalk.green(`El número ingresado es ${N}.\nDicho dígito es impar.`))
+    } 
+    }
+
+// ---------------------------------------------------------
+    
+let N = parseInt((prompt('Ingrese un número de 1 a 100: ')))
+
+// --------------------------------------------------------
+
+if (N >= 0 && N <= 100) {
+   esParImpar(N)
+} else {
+    console.log(chalk.redBright('El número ingresado no está entre 1 y 100, o no es un número!.'))
+}
+
+
+
+
+
--- 2_InstitucionEducacion.js
+++ 2_InstitucionEducacion.js
@@ -1,5 +1,45 @@
 // 2. Una institución de educación, con carreras tanto diurnas como vespertinas, considera lo siguiente en su proceso académico:
-// si un estudiante de carrera vespertina tiene una nota de presentación mayor o igual a 6, se exime, y si no alcanza el 6 pero tiene una nota de presentación igual o mayor a 3.5, rinde examen. 
+// si un estudiante de carrera vespertina tiene una nota de presentación mayor o igual a 6, 
+// se exime, y si no alcanza el 6 pero tiene una nota de presentación igual o mayor a 3.5, rinde examen. 
 // Si el estudiante es de carrera diurna y si su nota de presentación es mayor o igual a 3.5, rinde examen (no hay posibilidad de eximirse). 
 // En ambos regímenes, si la nota de presentación es menor que 3.5, reprueba. 
 // Hacer la tabla de decisión y escribir el algoritmo correspondiente.
+
+import PromptSync from "prompt-sync"
+import chalk from "chalk"
+
+const prompt = PromptSync()
+
+const carrera = prompt(`Indique la franja horaria de su Carrera (diurna o vespertina): `)
+
+const notaObtenida = parseFloat(prompt(`Indique la nota numérica que obtuvo en el examen: `))
+
+const franjaHoraria = carrera.toLowerCase()
+
+if (franjaHoraria === "vespertina") {
+    if (notaObtenida > 10 || notaObtenida < 11) {
+        console.log(chalk.red(`Nota fuera de rango`))
+    }
+    else if (notaObtenida >= 6 && notaObtenida <= 10) {
+              console.log(chalk.green(`Estudiante, usted se eximió.`))
+    }
+    else if (notaObtenida >= 3.5 && notaObtenida < 6) {
+        console.log(chalk.yellow(`Estudiante, usted rinde examen.`))
+    }
+    else {
+        console.log(chalk.red(`Estudiante, usted reprueba.`))
+    }
+
+} else if (franjaHoraria === "diurna") {
+    if (notaObtenida > 10 || notaObtenida < 11) {
+        console.log(chalk.red(`Nota fuera de rango`))
+    }
+    else if (notaObtenida >= 3.5 && notaObtenida <= 10){
+        console.log(chalk.yellow(`Estudiante, usted rinde examen.`))
+    } else {
+        console.log(chalk.red(`Estudiante, usted reprueba.`))
+    }
+} else {
+    console.log(chalk.red(`Franja horaria no válida`))
+}
+
